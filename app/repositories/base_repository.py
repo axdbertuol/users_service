03fd1a0e088cbc34f4619a47340092f7bf25b27a
axdbertuol <axdber@gmail.com>
2024-05-14 21:21:36 -0300

    refactor: raise instead of sending error

--- app/repositories/base_repository.py
+++ app/repositories/base_repository.py
@@ -1,9 +1,10 @@
 from fastapi.logger import logger
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
-from typing import Dict, Type, TypeVar, List, Optional
+from typing import Type, TypeVar, List, Optional
 
-from app.dtos.responses import ResponseModel
+from app.exceptions import DuplicatedError, InternalServerError
+
 
 # Create a generic repository class
 ModelType = TypeVar("ModelType")
@@ -14,106 +15,77 @@ class BaseRepository:
         self.session = session
         self.model_type = model_type
 
-    def get(self, id: int) -> Optional[Dict[str, Optional[ModelType]]]:
+    def get(self, id: int) -> Optional[ModelType]:
         try:
             result = (
                 self.session.query(self.model_type)
                 .filter(self.model_type.id == id)
                 .first()
             )
-            response_success = ResponseModel[Dict[str, Optional[ModelType]]](
-                success=True, data=result
-            )
 
-            return response_success
-        except SQLAlchemyError as e:
+            return result
+        except (SQLAlchemyError, IntegrityError) as e:
             logger.error(e)
-            response_failure = ResponseModel(success=False, error="Database error")
-            return response_failure
+            raise InternalServerError(e.args)
 
     def get_multi(
         self, skip: int = 0, limit: int = 10
-    ) -> ResponseModel[List[Dict[str, Optional[ModelType]]]]:
+    ) -> Optional[List[Optional[ModelType]]]:
         try:
             result = self.session.query(self.model_type).offset(skip).limit(limit).all()
-            response_success = ResponseModel[List[Dict[str, Optional[ModelType]]]](
-                success=True, data=result
-            )
-            return response_success
-        except SQLAlchemyError as e:
+            return result
+        except (SQLAlchemyError, IntegrityError) as e:
             logger.error("Database error: %s", e)
-            response_failure = ResponseModel(success=False, error="Database error")
-            return response_failure
+            raise InternalServerError(e.args)
 
-    def create(self, obj_in: dict) -> ResponseModel[Dict[str, Optional[ModelType]]]:
+    def create(self, obj_in: dict) -> ModelType:
         try:
             obj = self.model_type(**obj_in)
             self.session.add(obj)
             self.session.commit()
             self.session.refresh(obj)
 
-            response_success = ResponseModel[Dict[str, Optional[ModelType]]](
-                success=True, data=obj
-            )
-
-            return response_success
+            return obj
         except IntegrityError as e:
             self.session.rollback()  # Rollback changes in case of error
             if "unique constraint" in str(e):
                 logger.error("Duplicated entry: %s", e)
-                response_failure = ResponseModel(
-                    success=False, error="Duplicated entry"
-                )
-                return response_failure
+                raise DuplicatedError(e.args)
             else:
                 logger.error("Database error: %s", e)
-                response_failure = ResponseModel(success=False, error="Database error")
-                return response_failure
+                raise InternalServerError(e.args)
         except SQLAlchemyError as e:
             self.session.rollback()
             logger.error("Database error: %s", e)
-            response_failure = ResponseModel(success=False, error="Database error")
-            return response_failure
 
-    def update(self, db_obj: ModelType, obj_in: dict) -> ResponseModel:
+            raise InternalServerError()
+
+    def update(self, db_obj: ModelType, obj_in: dict) -> None:
         try:
             for key, value in obj_in.items():
                 setattr(db_obj, key, value)
             self.session.add(db_obj)
             self.session.commit()
             self.session.refresh(db_obj)
-            response_success = ResponseModel(success=True)
-            return response_success
+            return None
         except IntegrityError as e:
             self.session.rollback()  # Rollback changes in case of error
             if "unique constraint" in str(e):
                 logger.error("Duplicated entry: %s", e)
-                response_failure = ResponseModel(
-                    success=False, error="Duplicated entry"
-                )
-                return response_failure
+                raise DuplicatedError(e.args)
             else:
-                logger.error("Database error: %s", e)
-                response_failure = ResponseModel(success=False, error="Database error")
-                return response_failure
+                raise InternalServerError(e.args)
         except SQLAlchemyError as e:
             self.session.rollback()
             logger.error("Database error: %s", e)
-            return response_failure
+            raise InternalServerError(e.args)
 
-    def delete(self, db_obj: ModelType) -> ResponseModel:
+    def delete(self, db_obj: ModelType) -> None:
         try:
             self.session.delete(db_obj)
             self.session.commit()
-            response_success = ResponseModel(success=True)
-            return response_success
-        except IntegrityError as e:
+            return None
+        except (SQLAlchemyError, IntegrityError) as e:
             self.session.rollback()
             logger.error("Database error: %s", e)
-            response_failure = ResponseModel(success=False, error="Database error")
-            return response_failure
-        except SQLAlchemyError as e:
-            self.session.rollback()
-            logger.error("Database error: %s", e)
-            response_failure = ResponseModel(success=False, error="Database error")
-            return response_failure
+            raise InternalServerError(e.args)
