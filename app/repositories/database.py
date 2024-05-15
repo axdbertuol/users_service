3af85fbe613c9bde7277ba46872f5de2878ec6cd
axdbertuol <axdber@gmail.com>
2024-05-15 11:21:24 -0300

    fix: postgresql dbconn

--- app/repositories/database.py
+++ app/repositories/database.py
@@ -2,12 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+from app.config.constants import (
+    DATABASE_DBNAME,
+    DATABASE_PASSWORD,
+    DATABASE_USER,
+    DATABASE_HOST,
 )
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:5432/{DATABASE_DBNAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"sslmode": "disable"})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
