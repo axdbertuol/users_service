450cc89d2c1caba9ca8f048014d11590a61696d1
Alexandre Bertuol <axdbertuol@gmail.com>
2024-06-11 14:57:27 -0300

    feat: aioproducer to service

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,10 +1,10 @@
 from typing import Annotated, Any
 
 from fastapi import Depends
-
-from app.database import get_db
 from xeez_pyutils.sqlalchemy_repository import SQLAlchemyRepository
 
+from app.database import get_db
+from app.producer import get_aioproducer
 from app.users.models import User
 from app.users.repository import UserRepository
 from app.users.service import UserService
@@ -12,9 +12,10 @@ from app.users.service import UserService
 
 def make_user_router_deps(
     db: Annotated[Any, Depends(get_db)],
+    aioproducer: Annotated[Any, Depends(get_aioproducer)],
 ):
     base_repo = SQLAlchemyRepository(session=db, model_type=User)
     user_repo = UserRepository(base_repo)
-    user_service = UserService(user_repo=user_repo)
+    user_service = UserService(user_repo=user_repo, aioproducer=aioproducer)
 
     return user_service
--- app/users/service.py
+++ app/users/service.py
@@ -1,19 +1,47 @@
-# UserService to handle user-related operations
-
 from typing import List
 
-
+from aiokafka import AIOKafkaProducer
 from xeez_pyutils.common import CommonQueryParams
-from xeez_pyutils.exceptions import NotFoundError
+from xeez_pyutils.exceptions import NotFoundError, InternalServerError
+
 from .models import User
 from .protocols.service import UserServiceProtocol
-from .schemas import UserCreateIn, UserUpdateIn
 from .repository import UserRepository
+from .schemas import KafkaEvent, UserCreateIn, UserUpdateIn
+from .schemas import User as UserSchema
 
 
 class UserService(UserServiceProtocol):
-    def __init__(self, user_repo: UserRepository):
+    def __init__(self, user_repo: UserRepository, aioproducer: AIOKafkaProducer):
         self.user_repo = user_repo
+        self.aioproducer = aioproducer
+
+    async def create_user(self, body: UserCreateIn) -> User:
+        user = self.create_item(body)
+        try:
+            payload = UserSchema.model_validate(user).model_dump()
+            payload["user_id"] = user.id
+            value = KafkaEvent(
+                id=payload["email"],
+                type="user_created",
+                payload=payload,
+            ).model_dump_json()
+        except Exception as e:
+            raise InternalServerError(
+                "Something went wrong during processing of event", e
+            )
+
+        try:
+            await self.aioproducer.send(
+                topic="user-events",
+                value=value.encode("utf-8"),
+                key=payload["email"].encode("utf-8"),
+            )
+            return user
+        except Exception as e:
+            raise InternalServerError(
+                "Something went wrong during kafka message sending", e
+            )
 
     def create_item(self, body: UserCreateIn) -> User:
         user_dict = body.model_dump()
@@ -24,12 +52,62 @@ class UserService(UserServiceProtocol):
         created_user = self.user_repo.create(User(), user_dict)
         return created_user
 
+    async def update_user(self, item_id: int, body: UserUpdateIn) -> None:
+        self.update_item(item_id, body)
+        user = self.user_repo.get(User, item_id)
+        try:
+            payload = UserSchema.model_validate(user).model_dump()
+            payload["user_id"] = user.id
+            value = KafkaEvent(
+                id=user.email, type="user_updated", payload=payload
+            ).model_dump_json()
+        except Exception as e:
+            raise InternalServerError(
+                "Something went wrong during processing of event", e
+            )
+        try:
+            await self.aioproducer.send(
+                topic="user-events",
+                value=value.encode("utf-8"),
+                key=payload["email"].encode("utf-8"),
+            )
+            return user
+        except Exception as e:
+            raise InternalServerError(
+                "Something went wrong during kafka message sending", e
+            )
+
     def update_item(self, item_id: int, body: UserUpdateIn) -> None:
         user = self.user_repo.get(User, item_id)
         if user is None:
             raise NotFoundError
         user_dict = body.model_dump(exclude_unset=True)
         self.user_repo.update(user, user_dict)
+
+    async def delete_user(self, item_id: int) -> None:
+        self.delete_item(item_id)
+        user = self.user_repo.get(User, item_id)
+
+        try:
+            payload = {"user_id": user.id}
+            value = KafkaEvent(
+                id=user.email, type="user_deleted", payload=payload
+            ).model_dump_json()
+        except Exception as e:
+            raise InternalServerError(
+                "Something went wrong during processing of event", e
+            )
+
+        try:
+            await self.aioproducer.send(
+                topic="user-events",
+                value=value.encode("utf-8"),
+                key=payload["email"].encode("utf-8"),
+            )
+        except Exception as e:
+            raise InternalServerError(
+                "Something went wrong during kafka message sending", e
+            )
 
     def delete_item(self, item_id: int) -> None:
         user = self.user_repo.get(User, item_id)
